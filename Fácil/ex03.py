c1f7ba3d2c0c2f49185f330fc1266d824a2483bd
Washington Medeiros <wmedeirosm001@gmail.com>
2024-03-04 19:12:10 -0300

    Questão 4278

---
+++ Fácil/ex03.py
@@ -0,0 +1,12 @@
+
+
+# QUESTÃO 4278
+# "3° ÂNGULO DO TRIÂNGULO ADAP. P/AUTISMO"
+# NÍVEL DE ACORDO COM O THE HUXLEY: FÁCIL
+
+
+angulo_x = float(input())
+angulo_y = float(input())
+
+angulo_z = 180 - (angulo_x + angulo_y)
+print(angulo_z)
