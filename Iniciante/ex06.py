c09da247c17ab521d5374ac49bfce04e2eff1ae5
Washington Medeiros <wmedeirosm001@gmail.com>
2024-05-03 07:55:11 -0300

    QUESTÃO 1057 - "Teorema de Pitágoras"

---
+++ Iniciante/ex06.py
@@ -0,0 +1,12 @@
+
+
+# QUESTÃO 1057
+# "Teorema de Pitágoras"
+# NÍVEL DE ACORDO COM O THE HUXLEY: INICIANTE
+
+
+angulo1 = int(input())
+angulo2 = int(input())
+
+angulo3 = 180 - (angulo1 + angulo2)
+print(f'3o angulo={angulo3:.6f}')
