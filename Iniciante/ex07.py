6847ee70c0862e908648a4fde392edc8afb00a7e
Washington Medeiros <wmedeirosm001@gmail.com>
2024-05-03 07:57:52 -0300

    QUESTÃO 1057 - "Teorema de Pitágoras"

---
+++ Iniciante/ex07.py
@@ -0,0 +1,15 @@
+
+
+# QUESTÃO 1057
+# "Teorema de Pitágoras"
+# NÍVEL DE ACORDO COM O THE HUXLEY: INICIANTE
+
+
+from math import sqrt
+
+c1 = float(input())
+c2 = float(input())
+
+hip = sqrt(c1**2 + c2**2)
+
+print(f'{hip:.2f}')
