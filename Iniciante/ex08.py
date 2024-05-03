c507c5077302b8044cc8c9996aa9c6f19d0154ce
Washington Medeiros <wmedeirosm001@gmail.com>
2024-05-03 07:59:58 -0300

    QUESTÃO 1315 - "Bissexto"

---
+++ Iniciante/ex08.py
@@ -0,0 +1,13 @@
+
+
+# QUESTÃO 1315
+# "Bissexto"
+# NÍVEL DE ACORDO COM O THE HUXLEY: INICIANTE
+
+
+ano_bissexto = int(input())
+
+if ano_bissexto % 4 == 0:
+    print("Bissexto")
+else:
+    print("Nao bissexto")
