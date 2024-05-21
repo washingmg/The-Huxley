403d3c68b38b6e522c4528b0d4040faa9fc937d1
Washington Medeiros <wmedeirosm001@gmail.com>
2024-05-21 08:26:53 -0300

    QUESTÃO 2836 - "CONTAGEM REGRESSIVA (RECURSIVA)"

---
+++ Médio/ex11.py
@@ -0,0 +1,14 @@
+
+
+# QUESTÃO 2836
+# "CONTAGEM REGRESSIVA (RECURSIVA)"
+# NÍVEL DE ACORDO COM O THE HUXLEY: MÉDIO
+
+
+def contagem_regressiva(n):
+    while n > 0:
+        print(n)
+        n = n - 1
+    print('Decolar!')
+
+# contagem_regressiva(5)
