42e312ac86d93abca536d3d0dfeee3bcf1fcfd89
Washington Medeiros <wmedeirosm001@gmail.com>
2024-05-23 09:10:21 -0300

    QUESTÃO 2962 - "Troca de variáveis"

---
+++ Iniciante/ex17.py
@@ -0,0 +1,20 @@
+
+
+# QUESTÃO 2962
+# "Troca de variáveis"
+# NÍVEL DE ACORDO COM O THE HUXLEY: INICIANTE
+
+
+# O codigo para a entrada de dados e impressao
+# dos resultados eh dado.
+v1 = input()
+v2 = input()
+
+# insira na janela abaixo o codigo responsavel por
+# trocar os valores de uma variavel para a outra
+x, y = v1, v2
+v1 = y
+v2 = x
+
+print('valor em v1:', v1)
+print('valor em v2:', v2)
